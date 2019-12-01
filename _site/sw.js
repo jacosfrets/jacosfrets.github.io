ab8eb6d8e4a64294d94c6b0abdb066cd15cf175c
Aidan Wright <aawright@Aidans-MBP.telus>
2019-11-30 23:21:31 -0800

    Blog November30th

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20191129003804";
+var CACHE_NAME = "pixyll2-20191130231941";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?201911290038",
+        "/css/pixyll.css?201911302319",
         "/"
       ]);
     })
